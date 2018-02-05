414bd59383375eb9d0318a6e458d60311e5c05f9
suneelk14 <suneel.kesari14@github.com>
2018-02-05 16:37:26 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 var port = process.env.PORT || 3000;
-
 const publicPath = path.join(__dirname, "../public");
 var app = express() ;
 var server = http.createServer(app);
@@ -15,13 +14,32 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New User Connected');
 
+    socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+    } ) ;
+
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      message:'New user Joined',
+      createdAt: new Date().getTime()
+    });
+
+
     socket.on('createMessage', (message)=>{
       console.log('Received new Message', message) ;
+
       io.emit('newMessage', {
         from: message.from,
         message:message.text,
         createdAt: new Date().getTime()
-      })
+      });
+      // socket.broadcast.emit('newMessage', {
+      //   from: message.from,
+      //   message:message.text,
+      //   createdAt: new Date().getTime()
+      // });
     });
 
     socket.on('disconnect', ()=>{
